e0bead44a00cd454874b54917f8d765a19464dad
BenKoehler <ben.koehler@web.de>
2022-10-16 10:46:54 +0200

    replace CMAKE_CURRENT_SOURCE_DIR with PROJECT_SOURCE_DIR

--- bk.cmake
+++ bk.cmake
@@ -1,4 +1,4 @@
 function(link_googletest target MOD)
-    target_link_libraries(${target} ${MOD} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/googletest/lib/libgtest.so)
-    target_include_directories(${target} SYSTEM ${MOD} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/googletest/include)
+    target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/googletest/lib/libgtest.so)
+    target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/googletest/include)
 endfunction()
