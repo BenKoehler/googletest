2fd7d622a66b899481e62bbe905493a59283c26e
BenKoehler <ben.koehler@web.de>
2022-10-25 22:14:39 +0200

    add version macro

--- bk.cmake
+++ bk.cmake
@@ -1,3 +1,4 @@
+set(BK_GOOGLETEST_VERSION "1.12.1")
 function(link_googletest target MOD)
     target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/googletest/lib/libgtest.so)
     target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/googletest/include)
